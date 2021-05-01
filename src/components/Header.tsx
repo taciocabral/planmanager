6853924a6e481882bf2440d7ecdbcb129f461ad7
Tácio Cabral <tacio_cabral@yahoo.com.br>
2021-05-01 17:02:14 -0300

    - plant selector page; - pagination; - svg imgs & package

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, Image, View, StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/gitphoto.jpg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá</Text>
+                <Text style={styles.userName}>Tácio</Text>
+            </View>
+
+            <Image source={userImg} style={styles.image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 40
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+})
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/core';
 import {
     Text,
     View,
@@ -11,6 +12,11 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+    const navigation = useNavigation();
+
+    function handleMoveOn() {
+        navigation.navigate('PlantSelect')
+    }
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -27,7 +33,7 @@ export function Confirmation() {
                 </Text>
 
                 <View style={styles.footer}>
-                    <Button title='Começar ' />
+                    <Button title='Começar' onPress={handleMoveOn} />
                 </View>
             </View>
         </SafeAreaView>
